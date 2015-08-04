84418a357a342dba8b5174888ce7eb47e9fd0fa0
lekova <lina.lekova@gmail.com>
2015-08-04 14:36:22 -0400

    Initial commit
    Already has some boostrap in the index file

---
+++ assets/scripts/authenticate.js
@@ -0,0 +1,166 @@
+//jQuery.ajax
+$(function(){
+  'use strict';
+  var sa = '//localhost:3000';
+  //var sa = 'https://young-citadel-2431.herokuapp.com';
+  // var sa = 'http://10.13.108.54:3000';
+  var token;
+  var gameWatcher;
+
+  $("#register").on('click', function(e) {
+    $.ajax(sa + '/users', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#email').val(),
+          password: $('#password').val(),
+          password_confirmation: $('#password').val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data, textStatus, jqxhr) {
+      $('#result').val(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown) {
+      console.log("There was an error in authentication: " + jqxhr);
+      $('#result').val('registration failed');
+    });
+  });
+
+  $("#login").on('click', function(e){
+    $.ajax(sa + "/login", {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#email').val(),
+          password: $('#password').val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data){
+      console.log("Successfully authenticated");
+      $("#token").val(data.token);
+      $("#email").hide();
+      $("#password").hide();
+      $("#login").hide();
+      $("#register").hide();
+      $("signin-form").html("<button id=\"logout\" type=\"submit\" class=\"btn btn-success\">Logout</button>");
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      console.log("There was an error in authentication: " + jqxhr);
+      $('#result').val('login failed');
+    });
+  });
+
+  $("#list").on('click', function(e){
+    $.ajax(sa + '/users', {
+      dataType: 'json',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data){
+      $("#result").val(JSON.stringify(data));
+    }).fail(function(){
+      $("#result").val('list failed');
+    });
+  });
+
+  $("#create").on('click', function(e){
+    $.ajax(sa + '/games', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({}),
+      dataType: 'json',
+      method: 'POST',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data){
+      $("#result").val(JSON.stringify(data));
+    }).fail(function(){
+      $("#result").val('create failed');
+    });
+  });
+
+  $("#show").on("click", function(e){
+    $.ajax(sa + '/games/' + $("#id").val(), {
+      dataType: 'json',
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data, textStatus, jqxhr){
+      $("#result").val("");
+      $("#result").val(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      $("#result").val('show failed');
+    });
+  });
+
+  $("#join").on("click", function(e){
+    $.ajax(sa + '/games/' + $('#id').val(), {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({}),
+      dataType: 'json',
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data, textStatus, jqxhr){
+      $("#result").val("");
+      $("#result").val(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      $("#result").val('join failed');
+    });
+  });
+
+  $("#move").on("click", function(e){
+    $.ajax(sa + '/games/' + $('#id').val(), {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        game: {
+          cell: {
+            index: $("#index").val(),
+            value: $("#value").val()
+          }
+        }
+      }),
+      dataType: 'json',
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data, textStatus, jqxhr){
+      $("#result").val("");
+      $("#result").val(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      $("#result").val('move failed');
+    });
+  });
+
+  $("#watch").on("click", function(){
+    gameWatcher = resourceWatcher(sa + "/games/" + $("#id").val() + "/watch", {
+      Authorization: 'Token token=' + $('#token').val()
+    });
+    gameWatcher.on('change', function(data) {
+      var parsedData = JSON.parse(data);
+      // if (parsedData.timeout) { //not and error
+      //   gameWatcher.close();
+      //   return console.warn(parsedData.timeout);
+      // }
+      var gameData = parsedData.game;
+      var cell = gameData.cell;
+
+      $("#index").val(cell.index);
+      $("#value").val(cell.value);
+    });
+    gameWatcher.on('error', function(e) {
+      console.log("an error has occurred", e);
+    });
+  });
+});
---
+++ assets/scripts/main.js
@@ -0,0 +1,291 @@
+
+
+// $('#exampleModal').on('show.bs.modal', function (event) {
+//   var button = $(event.relatedTarget) // Button that triggered the modal
+//   var recipient = button.data('whatever') // Extract info from data-* attributes
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this)
+//   modal.find('.modal-title').text('New message to ' + recipient)
+//   modal.find('.modal-body input').val(recipient)
+// })
+
+var bookTemplate = function(book) {
+  var htmlString =  "<dt>Title </dt><dd>" + book.title + "</dd>" +
+                    "<dt>Author</dt><dd>" + book.author + "</dd>" +
+                    "<dt>year</dt><dd>" + book.year + "</dd>" +
+                    "<dt>Price</dt><dd>" + book.price + "</dd>" +
+                    "<dt>URL</dt><dd>" + book.url + "</dd>" +
+                    "<dt>Status</dt><dd>" + book.status + "</dd>";
+  return htmlString;
+};
+
+var getBooks = function(data){
+
+  if ((typeof data) === "string") {
+    data = JSON.parse(data);
+  };
+
+  console.log(data["books"]);
+
+  data["books"].forEach(function(book){
+    $("#book-label").append(bookTemplate(book));
+  });
+
+};
+
+//create book with Ajax
+$("#book-create").on("click", function() {
+  $.ajax({
+    url: 'http://localhost:3000/books',
+    method: 'POST',
+    headers: { Authorization: 'Token token=' + $('#token').val()},
+    data: {
+      book: {
+        title: $("#book-title").val(),
+        author: $("#book-author").val(),
+        year: $("#book-year").val(),
+        price: $("#book-price").val(),
+        url: $("#book-url").val()
+      }
+    }
+  }).done(function(book, textStatus, jqxhr){
+    console.log("Book created successfully");
+    console.log("textStatus: " +  textStatus);
+    console.log("jqxhr: " +  jqxhr);
+
+  $("#book-label").html(bookTemplate(book));
+  }).fail(function(jqxhr, textStatus, errorThrown){
+    console.log("There was an error while creating a book, error: " + jqxhr);
+    $("#book-label").html(jqxhr.responseText);
+  });
+});
+
+//show user books
+$("#books-my-show").on("click", function() {
+  $.ajax({
+    url: "http://localhost:3000/books?limit=me",
+    method: 'GET',
+    headers: { Authorization: 'Token token=' + $('#token').val()}
+  }).done(function(books, textStatus, jqxhr){
+    console.log("Listing all books for all users" + books);
+    console.log("textStatus: " +  textStatus);
+    console.log("jqxhr: " +  jqxhr);
+    getBooks(books);
+  }).fail(function(jqxhr, textStatus, errorThrown){
+    console.log("There was an error while LISTING CURRENT USER'S books, error: " + jqxhr);
+    $("#book-label").html(jqxhr.responseText);
+  });
+});
+
+// show all boks
+$("#books-show").on("click", function() {
+  $("#book-label").html();
+
+  $.ajax({
+    url: "http://localhost:3000/books",
+    method: 'GET' //,
+    // headers: { Authorization: 'Token token=' + $('#token').val()}
+  }).done(function(books, textStatus, jqxhr){
+    console.log("Listing all books for all users" + books);
+    console.log("textStatus: " +  textStatus);
+    console.log("jqxhr: " +  jqxhr);
+    getBooks(books);
+  }).fail(function(jqxhr, textStatus, errorThrown){
+    console.log("There was an error while LISTING ALL the books, error: " + jqxhr);
+    $("#book-label").html(jqxhr.responseText);
+  });
+});
+
+
+
+// update Book with Ajax
+$("#book-update").on("click", function() {
+  $.ajax({
+    url: 'http://localhost:3000/books/' + $("#book-id").val(),
+    method: 'PATCH',
+    headers: { Authorization: 'Token token=' + $('#token').val()},
+    data: {
+      book: {
+        title: $("#book-title").val(),
+        author: $("#book-author").val(),
+        year: $("#book-year").val(),
+        price: $("#book-price").val(),
+        url: $("#book-url").val(),
+        status: $("#book-status").val()
+      }
+    }
+  }).done(function(book, textStatus, jqxhr) {
+    $("#book-label").html(bookTemplate(book));
+  }).fail(function(jqxhr, textStatus, errorThrown) {
+    console.log("textStatus: " + textStatus);
+    $("#book-label").html(jqxhr.responseText);
+  });
+});
+
+//destroy Book with Ajax
+$("#book-destroy").on('click', function() {
+ $.ajax({
+   url: 'http://localhost:3000/books/' + $("#book-id").val(),
+   method: 'DELETE'
+ }).done(function(book, textStatus, jqxhr){
+   console.log("Your book has been deleted, here is some data about it: " + data);
+ }).fail(function(jqxhr, textStatus, errorThrown) {
+  $("#book-label").html(jqxhr.responseText);
+ });
+});
+
+// ------------------ Loan ---------------
+//create Loan with Ajax
+$("#loan-create").on('click', function() {
+  $.ajax({
+    url: 'http://localhost:3000/loans',
+    method: 'POST',
+    data: {
+      loan: {
+        name: $("#name").val(),
+        born: $("#born").val(),
+        died: $("#ded").val()
+      }
+    }
+  }).done(function(book, textStatus, jqxhr){
+    console.log("I'm a robot that created a person.");
+  }).fail(function(jqxhr, textStatus, errorThrown) {
+    $("#loan-label").html(jqxhr.responseText);
+  });
+});
+
+//update Loan with Ajax
+$("#loan-update").on('click', function(){
+  $.ajax({
+    url: 'http://localhost:3000/loans/' + $("#user-id").val(),
+    method: 'PATCH',
+    data: {
+      loan: {
+        name: $("#name").val(),
+        born: $("#born").val(),
+        died: $("#died").val()
+      }
+    }
+  }).done(function(book, textStatus, jqxhr){
+    console.log("I'm a robot that UPDATED a person.");
+  }).fail(function(jqxhr, textStatus, errorThrown) {
+    $("#loan-label").html(jqxhr.responseText);
+  });
+});
+
+//destroy Loan with Ajax
+$("#loan-destroy").on('click', function(){
+  $.ajax({
+    url: 'http://localhost:3000/loans/' + $("#user-id").val(),
+    method: 'DELETE'
+  }).done(function(book, textStatus, jqxhr){
+    console.log("This loan has been added to your list of deleted loans.");
+  }).fail(function(jqxhr, textStatus, errorThrown) {
+    $("#loan-label").html(jqxhr.responseText);
+  });
+});
+
+// ------------------ Role ---------------
+//create Role with Ajax
+$("#role-create").on('click', function(){
+ $.ajax({
+   url: '/roles',
+   method: 'POST',
+   data: {
+     role: {
+       name: $("#role-name").val(),
+       movie_id: $("#role-movie-id").val(),
+       person_id: $("#role-person-id").val()
+     }
+   }
+ }).done(function(book, textStatus, jqxhr){
+   console.log("I'm a robot that created a person.");
+ }).fail(function(data){
+   console.log("YOU DONE FUCKED UP NOW!");
+ });
+});
+
+//update Role with Ajax
+$("#role-update").on('click', function(){
+ $.ajax({
+   url: '/roles/' + $("#role-id").val(),
+   method: 'PATCH',
+   data: {
+     role: {
+       name: $("#role-name").val(),
+       movie_id: Number($("#role-movie-id").val()),
+       person_id: Number($("#role-person-id").val())
+     }
+   }
+ }).done(function(book, textStatus, jqxhr){
+   console.log("Chris is crazy about updating some roles around.");
+ }).fail(function(data){
+   console.log("YOU FUCKED UP NOW!");
+ });
+});
+
+//destroy Role with Ajax
+$("#role-destroy").on('click', function(){
+ $.ajax({
+   url: '/roles/' + $("#role-id").val(),
+   method: 'DELETE'
+ }).done(function(book, textStatus, jqxhr){
+   console.log("Chris hated this ROLE and got it DELETED.");
+ }).fail(function(data){
+   console.log("YOU FUCKED UP NOW!");
+ });
+});
+
+// ------------------ Review ---------------
+//create Review with Ajax
+$("#review-create").on("click", function() {
+ $.ajax({
+   url: '/reviews',
+   method: 'POST',
+   data: {
+     review: {
+       score: $("#review-score").val(),
+       content: $("#review-content").val(),
+       movie_id: $("#review-movie-id").val(),
+       user_id: $("#review-user-id").val()
+     }
+   }
+ }).done(function(book, textStatus, jqxhr){
+  console.log("Chris is a robot that created a review");
+ }).fail(function(date){
+  console.log("Everything has gone to hell with creating review!");
+ })
+});
+
+// update Review with Ajax
+$("#review-update").on("click", function() {
+ $.ajax({
+   url: '/reviews/' + $("#review-id").val(),
+   method: 'PATCH',
+   data: {
+     review: {
+       score: $("#review-score").val(),
+       content: $("#review-content").val(),
+       movie_id: Number($("#review-movie-id").val()),
+       user_id: Number($("#review-user-id").val())
+     }
+   }
+ }).done(function(book, textStatus, jqxhr){
+  console.log("Chris is a robot that UPDATED a review");
+ }).fail(function(date){
+  console.log("Everything has gone to hell with Updating this review!");
+ })
+});
+
+//destroy Review with Ajax
+$("#review-destroy").on('click', function(){
+ $.ajax({
+   url: '/reviews/' + $("#review-id").val(),
+   method: 'DELETE'
+ }).done(function(book, textStatus, jqxhr){
+   console.log("I'm a robot that DELETED a review.");
+ }).fail(function(data){
+  console.log("Everything has gone to hell with deleting this review!");
+ });
+});
