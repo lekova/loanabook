a53dc10afd6473f814c39e6064a98cc4e41ebc85
lekova <lina.lekova@gmail.com>
2015-08-06 23:29:33 -0400

    Add render display template
    Changes in render create, new result message in create.
    Delete of multiple unused functions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,8 @@ function showLoginForm() {
   $('#register-box').fadeOut('fast',function() {
       $('#login-box').fadeIn('fast');
       $('#modal-register-footer').fadeOut('fast',function() {
-          $('#model-label-header').text('Login');
-          $('#modal-login-footer').fadeIn('fast');
+        $('#model-label-header').text('Login');
+        $('#modal-login-footer').fadeIn('fast');
       });
   });
 };
@@ -12,8 +12,8 @@ function showRegisterForm() {
   $('#login-box').fadeOut('fast',function() {
       $('#register-box').fadeIn('fast');
       $('#modal-login-footer').fadeOut('fast',function() {
-          $('#model-label-header').text('Register');
-          $('#modal-register-footer').fadeIn('fast');
+        $('#model-label-header').text('Register');
+        $('#modal-register-footer').fadeIn('fast');
       });
   });
 };
@@ -22,7 +22,7 @@ var renderIndexPage = function() {
   var templatingFunction = Handlebars.compile($('#index-page-template').html());
   $.ajax({
     url: "http://localhost:3000/books",
-    method: 'GET' //,
+    method: 'GET'
   }).done(function(books, textStatus, jqxhr){
     console.log(books);
     var html = templatingFunction(books);
@@ -34,15 +34,29 @@ var renderIndexPage = function() {
   });
 };
 
+var setCreateResultMessage = function(success) {
+  if (success) {
+    $('#create-book-result-message').addClass('bg-success message');
+    $('#create-book-result-message').text('Your book was created successfully!');
+    $("#create-book-result-message").css();
+  } else {
+    $("#create-book-result-message").addClass('bg-danger message');
+    $("#create-book-result-message").text('Something went wrong!');
+  }
+}
+
 var booksPageTemplate = Handlebars.compile($("#books-page-template").html());
 
-var displayBook = function() {
-
+var renderCreateBookPage = function(book) {
+  var templatingFunction = Handlebars.compile($('#create-book-template').html());
+  var html = templatingFunction(book);
+  $("#page").html(html);
 };
 
-var renderCreateBookPage = function(title) {
-  var templatingFunction = Handlebars.compile($('#create-book-template').html());
-  var html = templatingFunction(title);
+var renderDisplayBookPage = function(book) {
+  var templatingFunction = Handlebars.compile($('#display-book-template').html());
+  var html = templatingFunction(book);
+
   $("#page").html(html);
 };
 
@@ -53,20 +67,29 @@ $(document).ready(function() {
 
   renderIndexPage("Books List");
 
-  $("#page").on("click", "#thumbnail-btn", function() {
-    var clicked = $(this).data('id');
-    console.log("CLICKED BUTTON NUMBER " + clicked);
+  $("#page").on("click", "#thumbnail-preview-btn", function() {
+    var clicked_id = $(this).data('id');
+    console.log("Clicked button number " + clicked_id);
+    $.ajax({
+      url: "http://localhost:3000/books/" + clicked_id,
+      method: 'GET'
+    }).done(function(book, textStatus, jqxhr){
+      console.log(book);
+      $("#page").html(renderDisplayBookPage(book));
+
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      console.log(jqxhr.responseText);
+    });
   });
 
   $("#create-a-book-dd-btn").on("click", function(){
     console.log("#create-a-book-dd-btn clicked");
-    renderCreateBookPage("Create new book");
+    renderCreateBookPage();
   });
 
   //create book with Ajax
   $("#page").on("click", "#book-create", function() {
     console.log("book create clicked");
-    debugger;
     $.ajax({
       url: 'http://localhost:3000/books',
       method: 'POST',
@@ -86,14 +109,18 @@ $(document).ready(function() {
       $("#book-title").text(book.title);
       $("#book-title").attr('readonly', 'readonly');
       $("#book-author").text(book.author);
+      $("#book-author").attr('readonly', 'readonly');
       $("#book-year").text(book.year);
+      $("#book-year").attr('readonly', 'readonly');
       $("#book-price").text(book.price);
-      // $("#book-summary").text(book.summary);
+      $("#book-price").attr('readonly', 'readonly');
       $("#book-url").text(book.url);
+      $("#book-url").attr('readonly', 'readonly');
+      setCreateResultMessage(true);
     }).fail(function(jqxhr, textStatus, errorThrown){
-
       console.log(textStatus);
       console.log(jqxhr.responseText);
+      setCreateResultMessage(false);
     });
   });
 
@@ -134,7 +161,7 @@ $(document).ready(function() {
     $.ajax({
       url: 'http://localhost:3000/books/' + $("#book-id").val(),
       method: 'PATCH',
-      headers: { Authorization: 'Token token=' + $('#token').val()},
+      headers: { Authorization: 'Token token=' + simpleStorage.get('token') },
       data: {
         book: {
           title: $("#book-title").val(),
@@ -214,109 +241,5 @@ $(document).ready(function() {
     }).fail(function(jqxhr, textStatus, errorThrown) {
       $("#loan-label").html(jqxhr.responseText);
     });
-  });
-
-  // ------------------ Role ---------------
-  //create Role with Ajax
-  $("#role-create").on('click', function(){
-   $.ajax({
-     url: '/roles',
-     method: 'POST',
-     data: {
-       role: {
-         name: $("#role-name").val(),
-         movie_id: $("#role-movie-id").val(),
-         person_id: $("#role-person-id").val()
-       }
-     }
-   }).done(function(book, textStatus, jqxhr){
-     console.log("I'm a robot that created a person.");
-   }).fail(function(data){
-     console.log("YOU DONE FUCKED UP NOW!");
-   });
-  });
-
-  //update Role with Ajax
-  $("#role-update").on('click', function(){
-   $.ajax({
-     url: '/roles/' + $("#role-id").val(),
-     method: 'PATCH',
-     data: {
-       role: {
-         name: $("#role-name").val(),
-         movie_id: Number($("#role-movie-id").val()),
-         person_id: Number($("#role-person-id").val())
-       }
-     }
-   }).done(function(book, textStatus, jqxhr){
-     console.log("Chris is crazy about updating some roles around.");
-   }).fail(function(data){
-     console.log("YOU FUCKED UP NOW!");
-   });
-  });
-
-  //destroy Role with Ajax
-  $("#role-destroy").on('click', function(){
-   $.ajax({
-     url: '/roles/' + $("#role-id").val(),
-     method: 'DELETE'
-   }).done(function(book, textStatus, jqxhr){
-     console.log("Chris hated this ROLE and got it DELETED.");
-   }).fail(function(data){
-     console.log("YOU FUCKED UP NOW!");
-   });
-  });
-
-  // ------------------ Review ---------------
-  //create Review with Ajax
-  $("#review-create").on("click", function() {
-   $.ajax({
-     url: '/reviews',
-     method: 'POST',
-     data: {
-       review: {
-         score: $("#review-score").val(),
-         content: $("#review-content").val(),
-         movie_id: $("#review-movie-id").val(),
-         user_id: $("#review-user-id").val()
-       }
-     }
-   }).done(function(book, textStatus, jqxhr){
-    console.log("Chris is a robot that created a review");
-   }).fail(function(date){
-    console.log("Everything has gone to hell with creating review!");
-   })
-  });
-
-  // update Review with Ajax
-  $("#review-update").on("click", function() {
-   $.ajax({
-     url: '/reviews/' + $("#review-id").val(),
-     method: 'PATCH',
-     data: {
-       review: {
-         score: $("#review-score").val(),
-         content: $("#review-content").val(),
-         movie_id: Number($("#review-movie-id").val()),
-         user_id: Number($("#review-user-id").val())
-       }
-     }
-   }).done(function(book, textStatus, jqxhr){
-    console.log("Chris is a robot that UPDATED a review");
-   }).fail(function(date){
-    console.log("Everything has gone to hell with Updating this review!");
-   })
-  });
-
-  //destroy Review with Ajax
-  $("#review-destroy").on('click', function(){
-   $.ajax({
-     url: '/reviews/' + $("#review-id").val(),
-     method: 'DELETE'
-   }).done(function(book, textStatus, jqxhr){
-     console.log("I'm a robot that DELETED a review.");
-   }).fail(function(data){
-    console.log("Everything has gone to hell with deleting this review!");
-   });
   });
 });
